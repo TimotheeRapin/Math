7c1cb37934ed98ef6d7e402defe7be17cc557229
TimotheeRapin <61775714+TimotheeRapin@users.noreply.github.com>
2023-05-17 10:00:34 +0200

    Initial commit

---
+++ Exercice 1-1.php
@@ -0,0 +1,37 @@
+<?php
+$A = ;
+$B = ;
+
+$X = ($A + $B) / 2;
+/*
+ * f(x)>0 ?
+ * $B = $X;
+ *
+ * dsc
+ * $A = $X;
+ *
+ * while($B - $A > $tolerance)
+ */
+
+/*
+$x=0;
+$n = ($x*3*3*3)-($x*2*2)+($x*3)-1;
+$tolerance = 0.000001; // tolérance d'erreur acceptable
+
+if ($n == 0) { // vérification si n est exactement égal à zéro
+    echo "La valeur de x est : " . $x . "\n";
+} else {
+    while (abs($n) > $tolerance) { // utilisation de la valeur absolue de n
+        $n = ($x*3*3*3)-($x*2*2)+($x*3)-1;
+        if ($n>0) {
+            $x+=0.00000000000000000000001;
+        } else if ($n<0) {
+            $x-=0.00000000000000000000001;
+        }
+        echo "\n" . "x = " . $x . "\n";
+        echo "n = " . $n . "\n";
+    }
+    echo "La valeur de x est : " . $x . "\n";
+}
+*/
+
