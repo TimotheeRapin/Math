b28805454807244a882f8d1c90d329bdbd5df6b7
TimotheeRapin <61775714+TimotheeRapin@users.noreply.github.com>
2023-05-24 10:40:19 +0200

    WIP

--- Exercice 1-1.php
+++ Exercice 1-1.php
@@ -1,63 +1,32 @@
 <?php
 $a = 0;
 $b = 1.5;
-
-
-$x = ($a + $b) / 2;
 $tolerance = 0.001;
 
-$f = $x^-4 - $x^4 - $x^3 + 4;
-
-/*
- * f(x)>0 ?
- * $B = $X;
- *
- * dsc
- * $A = $X;
- *
- * while($B - $A > $tolerance)
- */
-
-// Trouver f de x si x = 0
-while 
-
-while (abs($x) > $tolerance) {
-    $x = ($a + $b) / 2;
-
-    if ($x >= 0) {
-        $b = $x;
-    }
-    else {
-        $a = $x;
-    }
-
-    echo "x = " . $x . "\n";
-    echo "a = " . $a . "\n";
-    echo "b = " . $b . "\n\n";
+function f($x) {
+    $f = pow($x, -4) - pow($x, 4) - pow($x, 3) + 4;
+    return $f;
 }
 
-
-/*
-$n = ($X*3*3*3)-($X*2*2)+($X*3)-1;
-$tolerance = 0.000001; // tolérance d'erreur acceptable
-
-if ($n == 0) { // vérification si n est exactement égal à zéro
-    echo "La valeur de x est : " . $X . "\n";
+// Trouver f(x) si x = 0
+if (f($a) == 0) {
+    echo "La valeur de x est : " . $a . "\n";
 } else {
-    while (abs($n) > $tolerance) { // utilisation de la valeur absolue de n
-        $n = ($X*3*3*3)-($X*2*2)+($X*3)-1;
-        if ($n>0) {
-            $X+=$tolerance;
-        } else if ($n<0) {
-            $X-=$tolerance;
+    while (abs($b - $a) > $tolerance) {
+        $x = ($a + $b) / 2;
+
+        if (f($x) == 0) {
+            break;
         }
-        echo "\n" . "x = " . $X . "\n";
-        echo "n = " . $n . "\n";
+
+        if (f($x) * f($a) < 0) {
+            $b = $x;
+        } else {
+            $a = $x;
+        }
     }
-    echo "La valeur de x est : " . $X . "\n";
+    echo "La valeur de a est : " . $a . "\n";
+    echo "La valeur de b est : " . $b . "\n";
+    echo "La valeur de x est : " . $x . "\n";
+    echo "La valeur de f(x) est : " . f($x) . "\n";
 }
-*/
-
-/*
-$f($x) = $x^7 - $x^4 - $x^3 + 4;*/
-
