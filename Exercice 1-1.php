ef99e0aacb46211320806a930af7cc8955ac76cc
TimotheeRapin <61775714+TimotheeRapin@users.noreply.github.com>
2023-05-24 16:45:57 +0200

    Math

--- Exercice 1-1.php
+++ Exercice 1-1.php
@@ -1,32 +1,35 @@
 <?php
-$a = 0;
-$b = 1.5;
-$tolerance = 0.001;
+$a = 0.0;
+$b = 1.0;
+$e = 2.718282;
+$tolerance = 0.0000001;
 
 function f($x) {
-    $f = pow($x, -4) - pow($x, 4) - pow($x, 3) + 4;
+    //$f = pow($x, 3) - 5 * pow($x, 2) + 2;// $x-cos($x)
+    $f = pow(2.718282, -$x) - $x; //2.718282 = $e
     return $f;
 }
 
 // Trouver f(x) si x = 0
-if (f($a) == 0) {
-    echo "La valeur de x est : " . $a . "\n";
-} else {
-    while (abs($b - $a) > $tolerance) {
-        $x = ($a + $b) / 2;
-
-        if (f($x) == 0) {
-            break;
-        }
-
-        if (f($x) * f($a) < 0) {
-            $b = $x;
-        } else {
-            $a = $x;
-        }
-    }
-    echo "La valeur de a est : " . $a . "\n";
-    echo "La valeur de b est : " . $b . "\n";
-    echo "La valeur de x est : " . $x . "\n";
-    echo "La valeur de f(x) est : " . f($x) . "\n";
+if (f($a) > 0) {
+    $i = $b;
+    $b = $a;
+    $a = $i;
 }
+
+echo "La valeur de f(a) est : " . f($a) . "\n";
+while (abs($b - $a) > $tolerance) {
+    $x = ($a + $b) / 2.0;
+
+    if (f($x) > 0) {
+        $b = $x;
+    }
+    else {
+        $a = $x;
+    }
+}
+echo "La valeur de a est : " . $a . "\n";
+echo "La valeur de b est : " . $b . "\n";
+echo "La valeur de x est : " . $x . "\n";
+echo "La valeur de f(x) est : " . f($x) . "\n";
+
--- Exercice 2.php
+++ Exercice 2.php
@@ -1,10 +1,31 @@
 <?php
+// Trouver l'aire sous la courbe de f(x) = x^2 entre $a et $b
 
-$A = ;
-$B = ;
-$N = ;
+//$a = 0.0;
+$a = -6.0;
+//$b = 4.0;
+//$b = 3.14159;
+$b = 6.0;
+$nbColonnes = 100000;
 
-while ($N = 1 $){
-    $d = ($A + $B) / $N;
-    for ()
+$largeurColonne = ($b - $a) / $nbColonnes;
+$aire = 0.0;
+
+if (f($a) < 0) {
+    $i = $b;
+    $b = $a;
+    $a = $i;
 }
+
+function f($x) {
+    //$f = pow($x, 2) + 2;
+    //$f = sin($x);
+    $f = -pow($x, 2) + 36;
+    return $f;
+}
+
+for ($i = 0; $i < $nbColonnes; $i++) {
+    $aire += f($a + $i * $largeurColonne) * $largeurColonne;
+}
+
+echo "L'aire sous la courbe de f(x) = entre $a et $b est : " . $aire . "\n";
